ede35f4411a2a5b25d06de1d85491a5a49ae41ac
Cedric van Beijsterveldt <cedric.van.beijsterveldt@pm.me>
2020-01-13 22:40:38 +0100

    Refactored to use streams and functions, added unittests for individual items and modified TexttestFixture to no longer rely on mutable array state

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,14 @@
 package com.gildedrose;
 
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 class GildedRose {
+
+    private static final int DEGRADATION_RATE = 1;
+    private static final int MIN_QUALITY = 0;
+    private static final int MAX_QUALITY = 50;
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -8,55 +16,82 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        this.items = Stream.of(items)
+            .map(item -> getCorrectFunction(item).apply(item))
+            .toArray(Item[]::new);
+    }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+    private Function<Item, Item> getCorrectFunction(final Item item) {
+        switch (item.name) {
+            case "Aged Brie":
+                return updateAgingItem();
+            case "Sulfuras, Hand of Ragnaros":
+                return updateLegendaryItem();
+            case "Backstage passes to a TAFKAL80ETC concert":
+                return updateBackstagePass();
+            default:
+                return item.name.startsWith("Conjured") ? updateConjuredItem() : updateNormalItem();
         }
+    }
+
+    /**
+     * A normal item degrades linearly in quality as it is nearing its sell-in date.
+     * After said date, it degrades twice as fast.
+     * It will, however, never go above or below certain constant bounds.
+     */
+    private Function<Item, Item> updateNormalItem() {
+        return current -> new Item(current.name, current.sellIn - 1, calculateQualityLinearly(current, DEGRADATION_RATE));
+    }
+
+    /**
+     * A legendary item does not have to be sold and does not degrade in quality.
+     * It is also allowed to have a quality rating outside the normal ranges.
+     */
+    private Function<Item, Item> updateLegendaryItem() {
+        return current -> current;
+    }
+
+    /**
+     * An aging item increases in quality the older it gets.
+     * It will, however, never go above or below certain constant bounds.
+     */
+    private Function<Item, Item> updateAgingItem() {
+        return current -> new Item(current.name, current.sellIn - 1, calculateQualityLinearly(current, -DEGRADATION_RATE));
+    }
+
+    /**
+     * A backstage pass will increase in quality as its sell-in date comes closer to the date of the concert, but
+     * will drop to 0 after the concert.
+     * <p>
+     * Currently, the following rules apply:
+     * - Quality increases by 1 when there are more than 10 days left until the concert
+     * - Quality increases by 2 when there are 10 days or less until the concert
+     * - Quality increases by 3 when there are 5 days or less until the concert
+     * - Quality becomes 0 after the concert
+     */
+    private Function<Item, Item> updateBackstagePass() {
+        return current -> new Item(current.name, current.sellIn - 1, calculateQualityForBackstagePasses(current));
+    }
+
+    /**
+     * A conjured item works similar to a normal item, but degrades twice as fast.
+     */
+    private Function<Item, Item> updateConjuredItem() {
+        return current -> new Item(current.name, current.sellIn - 1, calculateQualityLinearly(current, DEGRADATION_RATE * 2));
+    }
+
+    private int calculateQualityLinearly(final Item item, final int degradation) {
+        return roundQualityWithinLimits(item.quality - (item.sellIn <= 0 ? 2 : 1) * degradation);
+    }
+
+    private int calculateQualityForBackstagePasses(final Item item) {
+        final int improvement = item.sellIn <= 0 ? -item.quality :
+            item.sellIn <= 5 ? 3 :
+                item.sellIn <= 10 ? 2 : 1;
+        return roundQualityWithinLimits(item.quality + improvement);
+    }
+
+    private int roundQualityWithinLimits(final int x) {
+        return x < MIN_QUALITY ? MIN_QUALITY : Math.min(x, MAX_QUALITY);
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,106 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    public void foo() {
+        final Item[] items = new Item[] { new Item("foo", 0, 0) };
+        final GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    public void updateNormalItemWithinSellIn() {
+        final Item[] items = new Item[]{new Item("foo", 1, 10)};
+        final GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("foo, 0, 9", app.items[0].toString());
+    }
+
+    @Test
+    public void updateNormalItemPastSellIn() {
+        final Item[] items = new Item[]{new Item("foo", 0, 10)};
+        final GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("foo, -1, 8", app.items[0].toString());
+    }
+
+    @Test
+    public void updatingNeverReducesQualityBelowZero() {
+        final Item[] items = new Item[]{new Item("foo", 0, 0)};
+        final GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("foo, -1, 0", app.items[0].toString());
+    }
+
+    @Test
+    public void updateAgingItem() {
+        final Item[] items = new Item[]{new Item("Aged Brie", 0, 10)};
+        final GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie, -1, 12", app.items[0].toString());
+    }
+
+    @Test
+    public void updatingNeverIncreasesQualityAboveFifty() {
+        final Item[] items = new Item[]{new Item("Aged Brie", 0, 50)};
+        final GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie, -1, 50", app.items[0].toString());
+    }
+
+    @Test
+    public void updateLegendaryItem() {
+        final Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 0, 80)};
+        final GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros, 0, 80", app.items[0].toString());
+    }
+
+    @Test
+    public void updateBackstagePassOverTenDays() {
+        final Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 15, 10)};
+        final GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert, 14, 11", app.items[0].toString());
+    }
+
+    @Test
+    public void updateBackstagePassBetweenTenAndFiveDays() {
+        final Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)};
+        final GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert, 9, 12", app.items[0].toString());
+    }
+
+    @Test
+    public void updateBackstagePassWithinFiveDays() {
+        final Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10)};
+        final GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert, 4, 13", app.items[0].toString());
+    }
+
+    @Test
+    public void updateBackstagePassForPastConcert() {
+        final Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)};
+        final GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert, -1, 0", app.items[0].toString());
+    }
+
+    @Test
+    public void updateConjuredItemWithinSellIn() {
+        final Item[] items = new Item[]{new Item("Conjured Mana Cake", 10, 10)};
+        final GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Conjured Mana Cake, 9, 8", app.items[0].toString());
+    }
+
+    @Test
+    public void updateConjuredItemPastSellIn() {
+        final Item[] items = new Item[]{new Item("Conjured Mana Cake", 0, 10)};
+        final GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Conjured Mana Cake, -1, 6", app.items[0].toString());
+    }
 }
